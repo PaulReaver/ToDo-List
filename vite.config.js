eef9599a7852db5bc81398e6b334bd5ed43ab4a9
Paul Vallas <vallasff@hotmail.com>
2023-03-18 15:18:59 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite'; // Provides intellisense without the need for jsdoc annotations
+
+export default defineConfig({
+    base: './', // Specifies base public path when served in development or production
+    build: {
+        outDir: 'docs', // Specifies the output directory
+    },
+});
