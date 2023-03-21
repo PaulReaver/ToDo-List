3a30c027390d812744e47bdaee8149125bc70dfe
Paul Vallas <vallasff@hotmail.com>
2023-03-21 13:15:21 +0200

    Import and call function that populates project list

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
+// Import CSS files
 import '../css/reset.css';
 import '../css/style.css';
+
+// Import modules
+import populateProjectList from './populate-project-list';
+
+populateProjectList();
