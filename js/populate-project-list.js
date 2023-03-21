083657d3246865ce6a791d9fc9c237c7a5ed1132
Paul Vallas <vallasff@hotmail.com>
2023-03-21 13:16:05 +0200

    Added function that adds projects

---
+++ js/populate-project-list.js
@@ -0,0 +1,17 @@
+import projectList from './project-list.js';
+
+export default function populateProjectList() {
+    for (let i = 0; i < projectList.length; i += 1) {
+        const li = document.createElement('li');
+        li.classList.add('project-item');
+        const projectName = document.createElement('h3');
+        projectName.classList.add('project-name');
+        projectName.textContent = `${projectList[i]}`;
+        const projectDeleteButton = document.createElement('button');
+        projectDeleteButton.classList.add('project-delete-button');
+        projectDeleteButton.textContent = '🗑';
+        li.append(projectName, projectDeleteButton);
+        const projectListUl = document.querySelector('.project-list-ul');
+        projectListUl.append(li);
+    }
+}
