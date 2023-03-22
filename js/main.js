3ab1d26ac9be9a94446c7f246d21a23cdfb1ff87
Paul Vallas <vallasff@hotmail.com>
2023-03-22 13:34:07 +0200

    Added event listener on submit project button

--- js/main.js
+++ js/main.js
@@ -5,4 +5,9 @@ import '../css/style.css';
 // Import modules
 import populateProjectList from './populate-project-list';
 
+const submitProjectInput = document.querySelector('#submit-project-input');
+submitProjectInput.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
 populateProjectList();
