42fc63b02974bc5b5c24932f837452b6e19a6571
Paul Vallas <vallasff@hotmail.com>
2023-03-22 13:34:20 +0200

    Changed projects to headings

--- js/populate-project-list.js
+++ js/populate-project-list.js
@@ -4,7 +4,7 @@ export default function populateProjectList() {
     for (let i = 0; i < projectList.length; i += 1) {
         const li = document.createElement('li');
         li.classList.add('project-item');
-        const projectName = document.createElement('button');
+        const projectName = document.createElement('h2');
         projectName.classList.add('project-name');
         projectName.textContent = `${projectList[i]}`;
         const projectDeleteButton = document.createElement('button');
