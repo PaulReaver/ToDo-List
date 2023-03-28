ba0a0fcdc5736a13e25b1fdac8a229cf2a083836
Paul Vallas <vallasff@hotmail.com>
2023-03-28 13:48:04 +0300

    Gets the name of the active project

---
+++ js/main-header.js
@@ -0,0 +1,6 @@
+export default function applyProjectHeader() {
+    const activeProject = document.querySelector(
+        '.active-project-item'
+    ).firstChild;
+    console.log(activeProject);
+}
