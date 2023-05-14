e2c962708cd39c723dccf7572a0ac292851e5b3e
Paul Vallas <vallasff@hotmail.com>
2023-05-14 22:35:57 +0300

    Changed simple strings to object names

--- js/project-list.js
+++ js/project-list.js
@@ -1,7 +1,19 @@
-const projectList = ['General'];
+const projectList = [];
+
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+    }
+}
+
+const general = new Project('General');
+projectList.push(general);
 
 export function addProject() {
-    const newProject = document.getElementById('add-project__input').value;
+    const newProjectName = document.getElementById('add-project__input').value;
+    const newProject = new Project(newProjectName);
+
     projectList.push(newProject);
 }
 
