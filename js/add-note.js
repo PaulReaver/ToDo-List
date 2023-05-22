d5ae1abb9b46503c7a2302d4e9373396c9febac4
Paul Vallas <vallasff@hotmail.com>
2023-05-22 21:30:29 +0300

    Adds note to selected project

---
+++ js/add-note.js
@@ -0,0 +1,29 @@
+import projectList from './project-list';
+
+class Note {
+    constructor(title, description, priority, date) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.date = date;
+    }
+}
+
+export default function addNote() {
+    const activeProject = document.querySelector(
+        '.project--active > .project__name'
+    ).textContent;
+
+    const title = document.getElementById('add-note__title').value;
+    const description = document.getElementById('add-note__description').value;
+    const priority = document.getElementById('add-note__priority').value;
+    const date = document.getElementById('add-note__date').value;
+
+    const note = new Note(title, description, priority, date);
+    for (let i = 0; i < projectList.length; i += 1) {
+        if (activeProject === projectList[i].name) {
+            projectList[i].tasks.push(note);
+            return;
+        }
+    }
+}
