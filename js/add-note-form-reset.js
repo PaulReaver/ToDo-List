62fa4838a544a328812768dc986870e9febc269b
Paul Vallas <vallasff@hotmail.com>
2025-01-12 15:10:49 +0200

    Added clearer documentation

--- js/add-note-form-reset.js
+++ js/add-note-form-reset.js
@@ -1,10 +1,12 @@
 const addNotePriority = document.querySelector('select');
 const addNoteDescription = document.querySelector('#add-note__description');
 
+// Resets the priority to the default "Low".
 export function resetPriorityToDefault() {
     addNotePriority.setAttribute('data-chosen', 'Low');
 }
 
+// Resets the height of the description textarea to its default height (58px).
 export function descriptionHeightReset() {
     addNoteDescription.style.height = '58px';
 }
